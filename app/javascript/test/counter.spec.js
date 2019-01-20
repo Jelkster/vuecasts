921666b7fd8c716b4feaa062c02e4cd33bc3e8bf
Ryan Jelks <jelkster@gmail.com>
2019-01-20 17:48:42 -0500

    testingvue: episode 3 - setting data and asserting styles
    https://laracasts.com/series/testing-vue/episodes/3

--- app/javascript/test/counter.spec.js
+++ app/javascript/test/counter.spec.js
@@ -11,12 +11,43 @@ describe ('Counter', () => {
     expect(wrapper.vm.count).toBe(0)
   })
 
-  it ('increments the count when the button is clicked', () => {
+  it ('increments the count when the increment button is clicked', () => {
     expect(wrapper.vm.count).toBe(0)
 
-    wrapper.find('button').trigger('click')
+    wrapper.find('.increment').trigger('click')
 
     expect(wrapper.vm.count).toBe(1)
+  })
+
+  // it ('decrements the count when the decrement button is clicked', () => {
+  //   expect(wrapper.vm.count).toBe(0)
+
+  //   wrapper.find('.increment').trigger('click') // 1
+  //   wrapper.find('.increment').trigger('click') // 2
+
+  //   wrapper.find('.decrement').trigger('click') // 1
+
+  //   expect(wrapper.vm.count).toBe(1)
+  // })
+
+  it ('decrements the count when the decrement button is clicked', () => {
+    wrapper.setData({ count: 5 })
+
+    wrapper.find('.decrement').trigger('click') // 4
+
+    expect(wrapper.vm.count).toBe(4)
+  })
+
+  it.only ('never goes below 0', () => {
+    expect(wrapper.vm.count).toBe(0)
+
+    expect(wrapper.find('.decrement').element.style.display).toBe('none')
+    expect(wrapper.find('.decrement').isVisible()).toBe(false)
+
+    // wrapper.find('.increment').trigger('click') // 1
+    wrapper.setData({ count: 1 })
+
+    expect(wrapper.find('.decrement').isVisible()).toBe(true)
   })
 
   it ('presents the current count', () => {
